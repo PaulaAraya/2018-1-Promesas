4e1b135dbac0c41c46e286d30e784471a5594994
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-09 22:11:41 -0300

    se agregan algunos movimientos laterales

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ function animateElement(element, start, target, duration){ //Retornará promesa 
     const delta = (target - start)*40/duration; //delta es lo que se debe mover por cuadro
     return new Promise((resolve, reject)=>{ // Acá se está declarando la promesa. Los parametros indican lo que resuelven y lo que se rechaza, cuando se resuelve se llama a resolve() y si no se llama a reject()
         const loop = setInterval(()=>{ // toma una funcion y la repite cada ciertos milisegundos
-            const current = start + counter++ * delta; //a acá indicamos el movimientoto, ++counter hace que sume y luego se multiplique. Counter ++ suma después. Formula = posición inicial + velocidad*tiempo
+            const current = start + counter++ * delta; //a acá indicamos el movimiento, ++counter hace que sume y luego se multiplique. Counter ++ suma después. Formula = posición inicial + velocidad*tiempo
             element.style.left = current;
-            if(start > target && current <= target){ // acá indicamos cuando queremos que finalize el moviento que seria alb llegar a target
+            if(start > target && current <= target){ // acá indicamos cuando queremos que finalize el moviento que seria al llegar a target
                 element.style.left = current;
                 clearInterval(loop); // Acá se termina la promesa
                 resolve();//Si queremos pasar una respuesta es a través del parámetro de resolve
@@ -27,26 +27,34 @@ function animateElement(element, start, target, duration){ //Retornará promesa 
 //Secuencial
 
 const allLi = document.getElementsByTagName("li");
-animateElement(allLi[0], -200, 200, 4000).then(()=>{ // coordenadas fuera de la pantalla se indican con numeros negativos. Acá se está haciendo uso de la promesa por fuera.
+/*animateElement(allLi[0], -200, 200, 4000).then(()=>{ // coordenadas fuera de la pantalla se indican con numeros negativos. Acá se está haciendo uso de la promesa por fuera.
     console.log("Terminó la animación de doge");
     return animateElement(allLi[1], -200, 200, 2000); //" cá tenemos una promesa anidada, es como decirle: hiciste esto, y ahora haz esto otro
 }).then(()=>{ 
     console.log("Terminó de llegar el cate");
 }).catch(()=>{
     console.log("Falló la animación");
-});
+});*/
 
 Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
     [
-        animateElement(allLi[1], -200, 600, 8000),
+        animateElement(allLi[1], -200, 1200, 8000),
         animateElement(allLi[0], -200, 600, 4000)
     ]
 ).then((results)=>{
+    console.log("Todas las animaciones siguen");
+    return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
+        [
+            animateElement(allLi[1], 1200, -200, 8000),
+            animateElement(allLi[0], 600, -200, 4000)
+        ]
+    )
+}).then((results)=>{
     console.log("Todas las animaciones terminaron");
     return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
         [
-            animateElement(allLi[1], 600, -200, 8000),
-            animateElement(allLi[0], 600, -200, 4000)
+            animateElement(allLi[1], -200, 600, 8000),
+            animateElement(allLi[0], -200, 600, 4000)
         ]
     )
 }).then(()=>{
