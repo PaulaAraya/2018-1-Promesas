efdeb1e24f1b46c18e1eb501a84846e499a6e0de
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-09 23:33:49 -0300

    se agrega nueva funcion y promesas para movimiento vertical

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-//La firma es el nombre de la función, los parámetros y lo que retorna
-function animateElement(element, start, target, duration){ //Retornará promesa con elemento
-    element.style.left = start; 
+//Función Movimiento Horizontal
+function animateElementHorizontal(elementHrz, startHrz, targetHrz, durationHrz){ //Retornará promesa con elemento
+    elementHrz.style.left = startHrz; 
     let counter = 0;
-    const delta = (target - start)*40/duration; //delta es lo que se debe mover por cuadro
+    const delta = (targetHrz - startHrz)*40/durationHrz; //delta es lo que se debe mover por cuadro
     return new Promise((resolve, reject)=>{ // Acá se está declarando la promesa. Los parametros indican lo que resuelven y lo que se rechaza, cuando se resuelve se llama a resolve() y si no se llama a reject()
         const loop = setInterval(()=>{ // toma una funcion y la repite cada ciertos milisegundos
-            const current = start + counter++ * delta; //a acá indicamos el movimiento, ++counter hace que sume y luego se multiplique. Counter ++ suma después. Formula = posición inicial + velocidad*tiempo
-            element.style.left = current;
-            if(start > target && current <= target){ // acá indicamos cuando queremos que finalize el moviento que seria al llegar a target
-                element.style.left = current;
+            const current = startHrz + counter++ * delta; //a acá indicamos el movimiento, ++counter hace que sume y luego se multiplique. Counter ++ suma después. Formula = posición inicial + velocidad*tiempo
+            elementHrz.style.left = current;
+            if(startHrz > targetHrz && current <= targetHrz){ // acá indicamos cuando queremos que finalize el moviento que seria al llegar a targetHrz
+                elementHrz.style.left = current;
                 clearInterval(loop); // Acá se termina la promesa
                 resolve();//Si queremos pasar una respuesta es a través del parámetro de resolve
-            }else if(start < target && current >= target){
-                element.style.left = current;
+            }else if(startHrz < targetHrz && current >= targetHrz){
+                elementHrz.style.left = current;
                 clearInterval(loop); // Acá se termina la promesa
                 resolve();//Si queremos pasar una respuesta es a través del parámetro de resolve
             }
         }, 40);// 40 es lo que se va a demorar en ejecutar la funcion de nuevo, entre cada llamada a la funcion
-    });                
+    });                  
 }
-
+//Función para movimiento vertical 
+function animateElementVertical(elementVrtc, startVrtc, targetVrtc, durationVrtc){ 
+    elementVrtc.style.top = startVrtc; // .top para los movimientos verticales
+    let counter = 0;
+    const delta = (targetVrtc - startVrtc)*40/durationVrtc; 
+    return new Promise((resolve, reject)=>{ 
+        const loop = setInterval(()=>{ 
+            const current = startVrtc + counter++ * delta; 
+            elementVrtc.style.top = current;
+            if(startVrtc > targetVrtc && current <= targetVrtc){ 
+                elementVrtc.style.top = current;
+                clearInterval(loop); 
+                resolve();
+            }else if(startVrtc < targetVrtc && current >= targetVrtc){
+                elementVrtc.style.top = current;
+                clearInterval(loop); 
+                resolve();
+            }
+        }, 40);
+    }); 
+}  
 // Somos programadoras de la promise
 //===================== Promise ===================
 // Somos las usuarias de la promise
@@ -27,38 +47,71 @@ function animateElement(element, start, target, duration){ //Retornará promesa 
 //Secuencial
 
 const allLi = document.getElementsByTagName("li");
-/*animateElement(allLi[0], -200, 200, 4000).then(()=>{ // coordenadas fuera de la pantalla se indican con numeros negativos. Acá se está haciendo uso de la promesa por fuera.
-    console.log("Terminó la animación de doge");
-    return animateElement(allLi[1], -200, 200, 2000); //" cá tenemos una promesa anidada, es como decirle: hiciste esto, y ahora haz esto otro
-}).then(()=>{ 
-    console.log("Terminó de llegar el cate");
-}).catch(()=>{
-    console.log("Falló la animación");
-});*/
+
 
 Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
     [
-        animateElement(allLi[1], -200, 1200, 8000),
-        animateElement(allLi[0], -200, 600, 4000)
+        animateElementHorizontal(allLi[1], -200, 1200, 8000), //movimiento de izq a derch
+        animateElementHorizontal(allLi[0], -200, 600, 4000)
     ]
 ).then((results)=>{
     console.log("Todas las animaciones siguen");
-    return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
+    return Promise.all( 
         [
-            animateElement(allLi[1], 1200, -200, 8000),
-            animateElement(allLi[0], 600, -200, 4000)
+            animateElementHorizontal(allLi[1], 1200, -200, 8000), //movimiento de derch a izq
+            animateElementHorizontal(allLi[0], 600, -200, 4000)
         ]
     )
 }).then((results)=>{
-    console.log("Todas las animaciones terminaron");
-    return Promise.all( // esto devuelve un arreglo de promesas y ejecutarlas a la vez, se resuelve cuansdo terminan todas las promesas.
+    console.log("Todas las animaciones se mueven horizontalmente");
+    return Promise.all( 
         [
-            animateElement(allLi[1], -200, 600, 8000),
-            animateElement(allLi[0], -200, 600, 4000)
+            animateElementHorizontal(allLi[1], -200, 600, 8000), //movimiento de izq a derch
+            animateElementHorizontal(allLi[0], -200, 1000, 4000)
+        ]
+    )
+}).then((results)=>{ //animación vertical
+    console.log("Todas las animaciones suben y bajan");
+    return Promise.all( 
+        [
+            animateElementVertical(allLi[1], 400, 200, 3000), // movimiento de abajo hacia arriba
+            animateElementVertical(allLi[0], 0, 400 , 3000) // movimiento de arriba hacia abajo
+        ]
+    )
+}).then((results)=>{
+    console.log("Todas las animaciones se mueven horizontalmente");
+    return Promise.all( 
+        [
+            animateElementHorizontal(allLi[1], 600, 900, 4000), //movimiento de izq a derch
+            animateElementHorizontal(allLi[0], 1000, 600, 4000) // derch a izq a derecha
+        ]
+    )
+}).then((results)=>{ //animación vertical
+    console.log("Todas las animaciones suben y bajan");
+    return Promise.all( 
+        [
+            animateElementVertical(allLi[1], 200, 400, 3000), // movimiento de arriba hacia arriba
+            animateElementVertical(allLi[0], 400, 200 , 3000) // movimiento de abajo hacia abajo
+        ]
+    )
+}).then((results)=>{
+    console.log("Todas las animaciones se mueven horizontalmente");
+    return Promise.all( 
+        [
+            animateElementHorizontal(allLi[1], 900, 600, 4000), //movimiento de izq a derch
+            animateElementHorizontal(allLi[0], 600, 1000, 4000) // derch a izq
+        ]
+    )
+}).then((results)=>{ //animación vertical
+    console.log("Todas las animaciones suben y bajan");
+    return Promise.all( 
+        [
+            animateElementVertical(allLi[1], 400, 200, 3000), // movimiento de abajo hacia arriba
+            animateElementVertical(allLi[0], 200, 400 , 3000) // movimiento de arriba hacia abajo
         ]
     )
 }).then(()=>{
-    console.log("Terminaron las animaciones de vuelta");
+    console.log("Terminaron las animaciones de dar vuelta");
 }).catch(()=>{
     console.log("Falló la animación");
 });
